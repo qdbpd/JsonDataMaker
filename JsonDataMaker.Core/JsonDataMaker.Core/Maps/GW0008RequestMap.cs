a7edbc2490713d707a71eb0c907551ebf5dcf506
Shigezo <u.sake32@gmail.com>
2019-05-09 17:54:11 +0900

    feat: GW0008:リクエスト用のパーサー作成中

---
+++ JsonDataMaker.Core/JsonDataMaker.Core/Maps/GW0008RequestMap.cs
@@ -0,0 +1,30 @@
+﻿using BankVision.WebAPI.Models.GW0008.Request;
+using CsvHelper.Configuration;
+
+namespace JsonDataMaker.Core.Maps
+{
+    /// <summary>
+    /// 預金口座残高照会RequestMap
+    /// </summary>
+    public class GW0008RequestMap : ClassMap<RequestMessageData>
+    {
+        public GW0008RequestMap()
+        {
+            // 店番
+            Map(x => x.YokinkozaZandakashokai.ShokaiKomoku.temban)
+                .Index(0).Name("temban");
+            // 科目コード
+            Map(x => x.YokinkozaZandakashokai.ShokaiKomoku.kamokuCode)
+                .Index(1).Name("kamokuCode");
+            // 口座番号
+            Map(x => x.YokinkozaZandakashokai.ShokaiKomoku.kozabango)
+                .Index(2).Name("kozabango");
+            // 過振区分
+            Map(x => x.YokinkozaZandakashokai.ShokaiKomoku.kaburiKubun)
+                .Index(3).Name("kaburiKubun");
+            // 契約者ID
+            Map(x => x.YokinkozaZandakashokai.KensaKomoku.keiyakushaId)
+                .Index(4).Name("keiyakushaId");
+        }
+    }
+}
---
+++ JsonDataMaker.Core/JsonDataMaker.Core/Parsers/GW0008RequestCsvParser.cs
@@ -0,0 +1,9 @@
+﻿using BankVision.WebAPI.Models.GW0008.Request;
+using JsonDataMaker.Core.Maps;
+
+namespace JsonDataMaker.Core.Parsers
+{
+    public class GW0008RequestCsvParser : CsvParser<RequestMessageData, GW0008RequestMap>
+    {
+    }
+}
